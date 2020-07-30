ddf0d06c6f03bd7160886a6b89f11a3b8a7fac90
Phil Hawksworth <phil@netlify.com>
2020-07-30 17:49:44 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = (eleventyConfig) => {
+
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  // A handy markdown shortcode for blocks of markdown
+  // coming from our data sources
+  const markdownIt = require('markdown-it');
+  const md = new markdownIt({
+    html: true
+  });
+  eleventyConfig.addFilter('markdown', (content) => {
+    return md.render(content);
+  });
+
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data"
+    }
+  };
+
+};
